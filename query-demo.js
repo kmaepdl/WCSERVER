1103423465bbce0e295dd5a3631d0797aa823012
kmaepdl <98503951+kmaepdl@users.noreply.github.com>
2022-03-02 23:28:27 +0800

    Delete query-demo.js

--- query-demo.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  console.log(req.query)
-})  
-app.listen(8080)
-
-//http://localhost:8080/?name=Kim&dept=Acctg&gender=F
