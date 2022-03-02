53d41ffa5581b30369ed5ca9895c83103a2eb15d
kmaepdl <98503951+kmaepdl@users.noreply.github.com>
2022-03-02 23:29:33 +0800

    Add files via upload

---
+++ first express/express-get/server.js
@@ -0,0 +1,28 @@
+//DE LEON, KIM
+//WD-201
+
+var express = require('express');  
+var app = express();  
+app.use(express.static('public'));  
+  
+app.get('/index.html', function (req, res) {  
+   res.sendFile( __dirname + "/" + "index.html" );  
+})  
+app.get('/process_get', function (req, res) {  
+response = {  
+       first_name:req.query.first_name,  
+       last_name: req.query.last_name,  
+       course:    req.query.course,
+       age:       req.query.age,
+       comment:   req.query.comment
+   };  
+   console.log(response);  
+   res.end(JSON.stringify(response));  
+})  
+var server = app.listen(5005, function () {  
+  
+  var host = server.address().address  
+  var port = server.address().port  
+  console.log("Example app listening at http://%s:%s", host, port)  
+  
+})
