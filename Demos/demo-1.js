2139f509694a8c73a73ff4868ec8a256e10e441c
kmaepdl <98503951+kmaepdl@users.noreply.github.com>
2022-03-02 23:32:44 +0800

    Add files via upload

---
+++ Demos/demo-1.js
@@ -0,0 +1,10 @@
+//De Leon, Kimberly Mae P.
+//WD-201
+//Creating a Function
+
+function Display(x) {
+  console.log(x)
+
+}
+
+Display(100);
---
+++ Demos/demo-2.js
@@ -0,0 +1,10 @@
+var http = require('http')
+
+var server = http.createServer(function(req, res){
+  
+  //any code here/path code
+
+
+});
+
+server.listen(5000);
