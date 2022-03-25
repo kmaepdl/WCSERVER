b655e5c1f16a5695c57d341b200fb7daee0c758a
kmaepdl <98503951+kmaepdl@users.noreply.github.com>
2022-03-25 23:41:54 +0800

    Add files via upload

---
+++ section1.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="section-1">
+    <div class="alert alert-dark" role="alert">
+        <h2>{{ msg1 }}</h2>
+    </div>
+        <ul>
+            </br>
+            <li><h3>This is the Profile Component</h3></li></br>
+            <li><h4>Objective: A hardworking and dedicated individual looking for a position to gain practical experience. To utilize and maximize my potentials.</h4></li></br>
+            <li><h4>Name: Kimberly Mae P. De Leon</h4></li></br>
+            <li><h4>Course: Information Technology with Specialization in Web Development</h4></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'section-1',
+    data () {
+        return {
+            msg1: ''
+        }
+    }
+}
+</script>
