5a64418b8b34a01225b809dfa12835f1f42d06b4
kmaepdl <98503951+kmaepdl@users.noreply.github.com>
2022-03-25 23:42:26 +0800

    Add files via upload

---
+++ section3.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="section-3">
+        <h2>{{ msg3 }}</h2>
+
+        <ul>
+            <li><h3>This is the Skills Component</h3></li></br>
+            <li><h4>HTML</h4></li>
+            <li><h4>CSS</h4></li>
+            <li><h4>JavaScript</h4></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'section-3',
+    data () {
+        return {
+            msg3: ''
+        }
+    }
+}
+</script>
